93ae7831ab3920637ae47199db1b931baac87398
Pierre <piergrimault@gmail.com>
2024-04-22 23:48:28 +0200

    Clean up

--- Tests/ExtensionsTests.cs
+++ Tests/ExtensionsTests.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine;
 using Xelareip;
 
 public class ExtensionsTests
@@ -13,6 +12,8 @@ public class ExtensionsTests
         const string normalString = "Foo";
 
         Assert.That(string.IsNullOrEmpty(emptyString) == emptyString.IsNullOrEmpty());
+        // ReSharper disable once ExpressionIsAlwaysNull
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
         Assert.That(string.IsNullOrEmpty(nullString) == nullString.IsNullOrEmpty());
         Assert.That(string.IsNullOrEmpty(normalString) == normalString.IsNullOrEmpty());
     }
@@ -21,37 +22,37 @@ public class ExtensionsTests
     public void RandomizeList()
     {
         const int testCount = 100; 
-        List<int> List0 = new List<int>();
-        List<int> List1 = new List<int>();
-        List<int> List2 = new List<int>();
+        List<int> list0 = new List<int>();
+        List<int> list1 = new List<int>();
+        List<int> list2 = new List<int>();
         
         for (int i = 0; i < testCount; ++i)
         {
-            List0.Add(i);
-            List1.Add(i);
-            List2.Add(i);
+            list0.Add(i);
+            list1.Add(i);
+            list2.Add(i);
         }
         
-        List0.Randomize();
-        List1.Randomize();
-        List2.Randomize();
+        list0.Randomize();
+        list1.Randomize();
+        list2.Randomize();
 
-        Assert.AreEqual(List0.Count, testCount);
-        Assert.AreEqual(List1.Count, testCount);
-        Assert.AreEqual(List2.Count, testCount);
+        Assert.AreEqual(list0.Count, testCount);
+        Assert.AreEqual(list1.Count, testCount);
+        Assert.AreEqual(list2.Count, testCount);
 
         float result = 0f;
         for (int i = 0; i < testCount; ++i)
         {
-            if (List0[i] == i)
+            if (list0[i] == i)
             {
                 ++result;
             }
-            if (List1[i] == i)
+            if (list1[i] == i)
             {
                 ++result;
             }
-            if (List2[i] == i)
+            if (list2[i] == i)
             {
                 ++result;
             }
